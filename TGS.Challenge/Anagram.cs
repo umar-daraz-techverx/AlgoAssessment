387bf5a3c00ae1980adc7caa01f2c6e72d8e0747
umar draz <u.draz@gems.techverx.com>
2021-04-16 14:48:36 +0500

    anagram code implemenations done

--- TGS.Challenge/Anagram.cs
+++ TGS.Challenge/Anagram.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Text.RegularExpressions;
+
 namespace TGS.Challenge
 {
   /*
@@ -24,7 +27,31 @@ namespace TGS.Challenge
     {
       public bool AreAnagrams(string word1, string word2)
       {
-        return false;
+	      if (string.IsNullOrEmpty(word1) || string.IsNullOrEmpty(word2))
+	      {
+		      throw new ArgumentException($"Neither string values can be null or empty to test a valid anagram");
+	      }
+
+	      word1 = RemoveSpecialCharacters(word1);
+	      word2 = RemoveSpecialCharacters(word2);
+
+	      var ch1 = word1.ToLower().ToCharArray();
+	      var ch2 = word2.ToLower().ToCharArray();
+
+	      if (ch1.Length != ch2.Length)
+		      return false;
+
+	      Array.Sort(ch1);
+	      Array.Sort(ch2);
+	     return new string(ch1).Equals(new string(ch2)) ? true : false;
+      }
+
+      //TODO: Explore to see if there is not a better way to remove white space and special caracters then using Regex(Performance!!)
+
+      //Private Regex to replace empty string and special caracters in the string
+      private static string RemoveSpecialCharacters(string input)
+      {
+	      return Regex.Replace(input, @"[^A-Za-z]", string.Empty);
       }
     }
 }
